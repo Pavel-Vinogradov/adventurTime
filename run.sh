41039a637ed6b9c0eb02f32ab8e6ea8d60229436
Pavel Vinogradov <pavelvinogradov45@gmail.com>
2024-05-30 22:30:12 +0300

    init

---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Обработка ошибок
+set -e
+
+# Запуск Docker Compose
+echo "Starting Docker Compose"
+docker-compose -f "docker-compose.yml" up --build --force-recreate --remove-orphans -d
+
+# Ожидание запуска контейнеров
+echo "Waiting for containers to be up and running..."
+sleep 10
+
+# Установка зависимостей Composer
+echo "Installing Composer dependencies"
+docker-compose exec adventure-time.php composer install
+
+
+echo "All done!"
---
+++ src/Core/Exception/ValidationException.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Core\Exception;
+
+use JsonException;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+
+final class ValidationException extends HttpException
+{
+    private array $errors;
+
+    /**
+     * @throws JsonException
+     */
+    public function __construct(array $errors, int $statusCode = Response::HTTP_UNPROCESSABLE_ENTITY)
+    {
+        $this->errors = $errors;
+        parent::__construct($statusCode, message: json_encode(['errors' => $errors], JSON_THROW_ON_ERROR));
+    }
+
+    public function getErrors(): array
+    {
+        return $this->errors;
+    }
+}
